bc62501b03ab77401557ef38acf512c2dc67dda7
Bryan Moreno <hbmorenog@gmail.com>
2020-12-11 11:33:57 -0500

    UpDate_Post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI,HTTPException
 from db import hotel_db
-from modelos import modelo_hotel
+from models import hotel_models
 
 app= FastAPI()
 
@@ -9,8 +9,8 @@ async def get_hoteles_api():
     return hotel_db.get_Hotels()
 
 @app.post("/hotel")
-async def auth_hotel(hotel_in: modelo_hotel.HotelIn): #mail query param
-    hotel_in_db = hotel_db.get_Hotel_mail(hotel_in.mail)
+async def auth_hotel(hotel_in: hotel_models.HotelIn): #email query param
+    hotel_in_db = hotel_db.get_Hotel_email(hotel_in.email)
 
     if hotel_in_db ==None:
         raise HTTPException(status_code=404,detail="El hotel no existe")
@@ -21,3 +21,13 @@ async def auth_hotel(hotel_in: modelo_hotel.HotelIn): #mail query param
 
     return {"Autenticado": True}
     
+@app.post("/hotel/agregar")
+async def add_hotel(hotel_add: hotel_models.HotelAdd):
+    hotel_add_db = hotel_db.enter_Hotel(hotel_add)
+
+    if hotel_add_db:
+        return {"Mensaje" : "Agregado satisfactoriamente"}
+    else:
+        raise HTTPException(status_code=400 , detail="El hotel ya existe")
+
+
