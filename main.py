d812b0be96b4da8c1f7e1cfda530da20e428eff0
ancagi <75188066+ancagi@users.noreply.github.com>
2020-12-10 22:30:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI,HTTPException
 from db import hotel_db
+from modelos import modelo_hotel
 
 app= FastAPI()
 
 @app.get("/hoteles/resumen")
 async def get_hoteles_api():
-    return contactos_db.get_Hotels()
+    return hotel_db.get_Hotels()
+
+@app.post("/hotel")
+async def auth_hotel(hotel_in: modelo_hotel.HotelIn): #mail query param
+    hotel_in_db = hotel_db.get_Hotel_mail(hotel_in.mail)
+
+    if hotel_in_db ==None:
+        raise HTTPException(status_code=404,detail="El hotel no existe")
+    
+    if hotel_in_db.password != hotel_in.password:
+
+        return {"Autenticado": False}
+
+    return {"Autenticado": True}
+    
