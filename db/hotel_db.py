8ee2460a55b8b827bd04cbef2924cdff2ae54b38
ancagi <ancagiraldo@gmail.com>
2020-12-10 21:47:38 -0500

    Funciones modificadas

--- db/hotel_db.py
+++ db/hotel_db.py
@@ -1,41 +1,51 @@
-from typing import Dict
 from pydantic import BaseModel
-from models.hotel_models import HotelOut
+
+
 class HotelInDB(BaseModel):
+    id: int
     mail: str
     password: str
     ubication: str
     name: str
     price: int 
 
-database_hotel= Dict[str, HotelInDB]
 
 database_hotel={
-    "decameron@gmail.com": HotelInDB(**{"mail":"decameron@gmail.com",
-                    "password":"123abc",
-                    "ubication":"Cartagena",
-                    "name":"Decameron",
-                    "price":250000}),
-    "hilton@gmail.com": HotelInDB(**{"mail":"hilton@gmail.com",
-                    "password":"root",
-                    "ubication":"Santa Marta",
-                    "name":"Hilton",
-                    "price":300000}),
-    "calypso@gmail.com": HotelInDB(**{"mail":"calypso@gmail.com",
-                    "password":"admin123",
-                    "ubication":"San Andres",
-                    "name":"Calypso Beach",
-                    "price":280000}),
+    1: HotelInDB(id=1,mail="decameron@gmail.com",
+                    password="123abc",
+                    ubication="Cartagena",
+                    name="Decameron",
+                    price=250000),
+    2: HotelInDB(id=2,mail="hilton@gmail.com",
+                    password="root",
+                    ubication="Santa Marta",
+                    name="Hilton",
+                    price=300000),
+    3: HotelInDB(id=3,mail="calypso@gmail.com",
+                    password="admin123",
+                    ubication="San Andres",
+                    name="Calypso Beach",
+                    price=280000)
 
 }
 
-def get_Hotel(mail:str):
-    if mail in database_hotel.keys():
-        return database_hotel[mail]
+def get_Hotels():
+    lista_hoteles=[]
+    for hotel in database_hotel:
+        lista_hoteles.append(database_hotel[hotel])
+    return lista_hoteles
+
+def get_Hotel_id(id):
+    if id in database_hotel:
+        return database_hotel[id]
     else:
         return None
 
-def update_hotel(hotel_in_db : HotelInDB):
-    database_hotel[hotel_in_db.mail] = hotel_in_db
-    return hotel_in_db
+def enter_Hotel(hotel_in_db: HotelInDB):
+    if hotel_in_db.email in database_hotel:
+        return False
+    else:
+        database_hotel[hotel_in_db.mail]=hotel_in_db
+        return True
 
+        
